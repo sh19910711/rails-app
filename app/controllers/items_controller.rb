362daf07969951d6350d0db59d2fc9ded27ebcdb
Hiroyuki Sano <sh19910711@gmail.com>
2016-08-10 15:04:59 +0900

    implement item actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,31 @@
 class ItemsController < ApplicationController
+  layout 'application'
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      redirect_to items_path
+    end
+  end
+
+  private
+  
+    def item_params
+      params.require(:item).permit(:name)
+    end
 end
